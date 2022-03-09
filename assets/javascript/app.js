af25663528e566fdcdef50e2e937a298b08c4f52
asunfavour <asunfavour03@gmail.com>
2022-03-09 10:20:40 +0100

    CONSTRUCTOR FUNCTIONS-- the this keyword was used here

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,6 +23,33 @@ var myCar2 = {
     }
 
 };
+// myCar2.logDriver();
+// console.log(myCar2.maxSpeed);
+// myCar2.drive(50, 3);
+
+var Car = function(maxSpeed, driver){
+
+    this.maxSpeed = maxSpeed;
+    this.driver = driver;
+    this.drive = function(speed, time){
+        console.log(speed * time);
+    };
+
+    this.logDriver= function(){
+        console.log("driver name is " + this.driver);
+    }
+
+
+}
+
+var myCar = new Car(70, "ninja man");
+var myCar2 = new Car(70, " man");
+var myCar3 = new Car(70, "shawn man");
+var myCar4 = new Car(70, "many man");
+
+myCar.drive(30,5);
+myCar2.drive(30,6);
 myCar2.logDriver();
-console.log(myCar2.maxSpeed);
-myCar2.drive(50, 3);
+myCar3.logDriver();
+myCar4.drive(30,7);
+
