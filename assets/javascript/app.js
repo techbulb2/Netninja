c69ae413b22ee439b13fe6a618a1d9080a8c4dea
asunfavour <asunfavour03@gmail.com>
2022-03-15 09:31:11 +0100

    javascript timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,50 @@
 
 
-window.onload = function(){
+// window.onload = function(){
 
-    var content = document.getElementById("content");
-    var button = document.getElementById("show-more");
+//     var content = document.getElementById("content");
+//     var button = document.getElementById("show-more");
 
-    button.onclick = function(){
-        if (content.className == "open"){
-            //shrink the box
-            content.className = "";
-            button.innerHTML = "show more";
-        }else {
-            //expand the box
-            content.className = "open";
-            button.innerHTML = "show less";
-        }
-    };
+//     button.onclick = function(){
+//         if (content.className == "open"){
+//             //shrink the box
+//             content.className = "";
+//             button.innerHTML = "show more";
+//         }else {
+//             //expand the box
+//             content.className = "open";
+//             button.innerHTML = "show less";
+//         }
+//     };
 
+// };
+
+//JAVASCRIPT TIMER
+
+// var myMessage = document.getElementById("message");
+
+// function showMessage(){
+//     myMessage.className = "show";
+// }
+
+// setTimeout(showMessage, 3000);
+
+var colorChanger = document.getElementById("color-changer");
+var colours = ["red", "blue", "green", "pink"];
+var counter = 0;
+
+function changecolor (){
+    if (counter >= colours.length){
+        counter = 0;
+    }
+
+    colorChanger.style.background = colours[counter];
+    counter++;
+};
+
+var myTimer = setInterval(changecolor, 3000);
+
+colorChanger.onclick = function(){
+    clearInterval(myTimer);
+    colorChanger.innerHTML = "timer stopped."
 };
