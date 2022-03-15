36d1b131d9eebbb0b09e54b9815a81faf5df72b0
asunfavour <asunfavour03@gmail.com>
2022-03-15 09:07:58 +0100

    making some changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,20 @@
-var content = document.getElementById("content");
-var button = document.getElementById("show-more");
 
-button.onclick = function(){
-    if (content.className == "open"){
-        //shrink the box
-        content.className = "";
-        button.innerHTML = "show more";
-    }else {
-        //expand the box
-        content.className = "open";
-        button.innerHTML = "show less";
-    }
+
+window.onload = function(){
+
+    var content = document.getElementById("content");
+    var button = document.getElementById("show-more");
+
+    button.onclick = function(){
+        if (content.className == "open"){
+            //shrink the box
+            content.className = "";
+            button.innerHTML = "show more";
+        }else {
+            //expand the box
+            content.className = "open";
+            button.innerHTML = "show less";
+        }
+    };
+
 };
